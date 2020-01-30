b53279c25e04b9db2479d9759a545df6ed3e5727
Hunter Parcells <hparcells@icloud.com>
2020-01-30 14:37:16 -0500

    Exported CTX and Canvas

--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -1,18 +1,8 @@
-/** Setups the canvas render cycle. */
-export function doCanvas() {
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-  if(!canvas) {
-    throw new Error('No canvas.');
-  }
-  const ctx = canvas.getContext('2d');
-  if(!ctx) {
-    throw new Error('No context.');
-  }
-  
-  // Set the canvas to the whole screen.
-  ctx.canvas.width  = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
-  
+export let canvas: HTMLCanvasElement;
+export let ctx: CanvasRenderingContext2D;
+
+/** The game loop. */
+function gameLoop() {
   // Resize the canvas when we resize the canvas.
   window.addEventListener('resize', () => {
     ctx.canvas.width  = window.innerWidth;
@@ -23,6 +13,25 @@ export function doCanvas() {
   ctx.fillStyle = '#000000';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Take it back now tall y'all.
-  window.requestAnimationFrame(doCanvas);
+  // Take it back now y'all.
+  window.requestAnimationFrame(gameLoop);
+}
+
+/** Setups the canvas render cycle. */
+export function doCanvas() {
+  canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  if(!canvas) {
+    throw new Error('No canvas.');
+  }
+  ctx = canvas.getContext('2d') as any;
+  if(!ctx) {
+    throw new Error('No context.');
+  }
+  
+  // Set the canvas to the whole screen.
+  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+  
+  // Start the game loop.
+  window.requestAnimationFrame(gameLoop);
 }
